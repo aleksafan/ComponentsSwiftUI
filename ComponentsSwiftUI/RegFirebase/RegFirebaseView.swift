b2db5b4f8765dd715dffc73294b488c6ff15f987
millioncats <a.n.afanasieva@ya.ru>
2020-11-10 13:30:47 -0500

    Creating

---
+++ ComponentsSwiftUI/RegFirebase/RegFirebaseView.swift
@@ -0,0 +1,23 @@
+//
+//  RegFirebaseView.swift
+//  ComponentsSwiftUI
+//
+//  Created by Aleksandra Afanasieva on 2020-11-10.
+//
+
+import SwiftUI
+
+struct RegFirebaseView:View{
+    var body: some View{
+        Form{
+            Text("Registration with Firebase")
+        }
+    }
+    
+}
+
+struct RegFirebaseView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegFirebaseView()
+    }
+}
