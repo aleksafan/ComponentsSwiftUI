6698bd2a5c14ac2c6a67114b367124814fb130cf
millioncats <a.n.afanasieva@ya.ru>
2020-11-10 13:18:08 -0500

    Initial Commit

---
+++ ComponentsSwiftUI/ComponentsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsSwiftUIApp.swift
+//  ComponentsSwiftUI
+//
+//  Created by Aleksandra Afanasieva on 2020-11-10.
+//
+
+import SwiftUI
+
+@main
+struct ComponentsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
