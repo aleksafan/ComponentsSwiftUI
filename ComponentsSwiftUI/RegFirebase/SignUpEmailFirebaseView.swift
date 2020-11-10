10c56e1bc580d41cab7ebbd69c9e715303d7543e
millioncats <a.n.afanasieva@ya.ru>
2020-11-10 18:10:16 -0500

    View structure created

---
+++ ComponentsSwiftUI/RegFirebase/SignUpEmailFirebaseView.swift
@@ -0,0 +1,54 @@
+//
+//  RegFirebaseView.swift
+//  ComponentsSwiftUI
+//
+//  Created by Aleksandra Afanasieva on 2020-11-10.
+//
+
+import SwiftUI
+
+struct SignUpEmailFirebaseView:View{
+    @State var email = ""
+    @State var pass = ""
+    @State private var isAlert:Bool = false
+    @State private var errorMessage:String? = nil
+    @State private var hidePass:Bool = true
+    var body: some View{
+        VStack{
+            Text("Registration with Firebase and e-mail")
+            
+            VStack{
+                Form {
+                    TextField("e-mail", text: self.$email)
+                        .keyboardType(.emailAddress)
+                    HStack {
+                        if self.hidePass{
+                            SecureField("Password", text: self.$pass)
+                            
+                        }else{
+                            TextField("Password", text: self.$pass)
+                        }
+                        Image(systemName: self.hidePass ? "eye.slash" : "eye").onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
+                            self.hidePass.toggle()
+                        }
+                        )
+                    }
+                }
+                Button(action: self.SignUpEmail){
+                    Text("Sign Up")
+                }
+                
+                
+            }
+        }
+    }
+    func SignUpEmail(){
+        
+    }
+}
+
+struct SignUpEmailFirebaseView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpEmailFirebaseView()
+    }
+}
--- ComponentsSwiftUI/Styles.swift
+++ ComponentsSwiftUI/Styles.swift
@@ -6,3 +6,16 @@
 //
 
 import SwiftUI
+
+struct OkButtonStyle: ButtonStyle {
+    func makeBody (configuration: Self.Configuration) -> some View{
+        configuration.label
+            .font(.headline)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            .contentShape(Rectangle())
+            .foregroundColor(configuration.isPressed ? Color.white.opacity(0.5) : Color.white)
+            .listRowBackground(configuration.isPressed ? Color.blue.opacity(0.5) : Color.blue)
+    }
+}
+
+
